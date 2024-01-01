e40ab4e86330f44c2de0ed7957a5173f02b632ea
broosters <154639926+broosters@users.noreply.github.com>
2024-01-01 20:59:50 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,11 +13,14 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
         </div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <div className={styles.externalLinks}>
-            <a href="https://github.com/gamba-labs/gamba" target="_blank" rel="noreferrer">
-              Github
+            <a href="https://twitter.com/broostersol" target="_blank" rel="noreferrer">
+              Twitter
             </a>
-            <a href="https://explorer.gamba.so" target="_blank" rel="noreferrer">
-              Stats
+            <a href="https://discord.com/invite/broosters" target="_blank" rel="noreferrer">
+              Discord
+            </a>
+            <a href="SOON" target="_blank" rel="noreferrer">
+           <font color="blue">   POINTS</font>
             </a>
           </div>
           {children}
